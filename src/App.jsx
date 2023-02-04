623c53946a595f97e469ddb6903de91042fd8f50
matisagardia <matisagardia7@gmail.com>
2023-02-04 14:39:57 -0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Navbar } from "./components/Navbar"
 import { Home } from "./components/Home"
 import { Projects } from "./components/Projects"
 import { Skills } from "./components/Skills"
-import "./style.css";
 
 function App() {
 
