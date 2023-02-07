b2499c5b559489fa87a9156672d1a9ff90364311
matisagardia <matisagardia7@gmail.com>
2023-02-07 12:05:25 -0300

    smaller projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,9 @@
 import { FcBriefcase } from "react-icons/fc";
 import { BsCarousel } from "./BsCarousel";
+import heroesApp from  "../assets/projects/heroes.png"
+import gifApp from "../assets/projects/gifexpert.png"
+import pig from "../assets/projects/pig.png"
+import guess from "../assets/projects/guess.png"
 
 export const Projects = () => {
   return (
@@ -15,19 +19,23 @@ export const Projects = () => {
         </div>
 
         <div className="smallerProjects">
-            <p>Some smaller projects/apps</p>
+            <p className="smallerTitle">Some smaller projects/apps</p>
             <div className="smallerContainer">
                 <div className="app1">
                     <h3>Heroes App</h3>
+                    <img src={heroesApp} alt=""/>
                 </div>
                 <div className="app2">
                     <h3>Gifs App</h3>
+                    <img src={gifApp} alt="" />
                 </div>
                 <div className="app3">
                     <h3>Pig Game</h3>
+                    <img src={pig} alt="" />
                 </div>
                 <div className="app4">
                 <h3>Guess my Number</h3>
+                <img src={guess} alt="" />
                 </div>
             </div>
         </div>
