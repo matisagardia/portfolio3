be488b527e22373e9111503801659a3229bed0aa
matisagardia <matisagardia7@gmail.com>
2023-02-07 09:29:18 -0300

    comments finished

--- src/components/BsCarousel.jsx
+++ src/components/BsCarousel.jsx
@@ -14,6 +14,8 @@ import bootstrap from "../assets/skillslogos/bootstrap.png"
 import mongo from "../assets/skillslogos/mongo.png"
 import redux from "../assets/skillslogos/redux.png"
 import git from "../assets/skillslogos/git.png"
+import firebase from "../assets/skillslogos/firebase.png"
+import mui from "../assets/skillslogos/mui.png"
 
 
 
@@ -36,46 +38,56 @@ export  const BsCarousel = () => {
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>
+      <Carousel.Item id='carrouselJournal'>
       <div className="carrousel">
-      <img src={journalApp} alt="" />
+      <img src={journalApp} alt="" className='journal'/>
         </div>
-        <Carousel.Caption>
-          <h5>Journal App</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className="carrousel">
-      <img src={CalendarPage} alt="" />
-        </div>
-        <Carousel.Caption>
-          <h5>Calendar App</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <Carousel.Caption className='journalDescription'>
+          <h1>Journal App</h1>
+          <img src={html} alt="" />
+          <img src={css} alt="" />
+          <img src={js} alt="" />
+          <img src={react} alt="" />
+          <img src={redux} alt="" />
+          <img src={firebase} alt="" />
+          <img src={mui} alt="" />
+          <img src={git} alt="" />
+          <h4>
+            Register and Login implemented with Firebase Auth. It has Material UI for the icons/style. It is a journal, in which the user can add/edit/delete tasks and also add images. The images are hosted in cloudinary and the notes use Firebase Database. The state is managed with Redux Toolkit.
+          </h4>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>
-       <div className="carrousel">
-          <img src={BankistLanding} alt="" />
-       </div>
-        <Carousel.Caption>
-          <h5>Bankist Landing</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <Carousel.Item id='carrouselCalendar'>
+      <div className="carrousel">
+      <img src={CalendarPage} alt="" className='calendar'/>
+        </div>
+        <Carousel.Caption className='calendarDescription'>
+          <h1>Calendar App</h1>
+          <img src={html} alt="" />
+          <img src={css} alt="" />
+          <img src={js} alt="" />
+          <img src={react} alt="" />
+          <img src={redux} alt="" />
+          <img src={mongo} alt="" />
+          <img src={node} alt="" />
+          <img src={git} alt="" />
+          <h4>
+            This is a calendar app to add tasks with initial and end date which accepts different users. The library for the calendar is React Big Calendar and React Date Picker for the dates. It uses MongoDB to store the users and notes information and Redux Toolkit to manage the state. For the backend, Node.js and Express were implemented.
+          </h4>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>
+      <Carousel.Item id='carrouselBankSimulator'>
        <div className="carrousel">
-          <img src={BankingSimulator} alt="" />
+          <img src={BankingSimulator} alt="" className='bankSimulator'/>
        </div>
-        <Carousel.Caption>
-          <h5>Bank Simulator</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className='bankSimulatorDescription'>
+          <h1>Bank Simulator</h1>
+          <img src={html} alt="" />
+          <img src={css} alt="" />
+          <img src={js} alt="" />
+          <h4>App to simulate a bank account. You can transfer money between accounts, request a loan, and close the account. Also, you can sort the movements. The login/logout function is only visibility by changing the opacity of a screen.</h4>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
